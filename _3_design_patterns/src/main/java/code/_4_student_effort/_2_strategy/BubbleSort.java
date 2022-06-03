58b2423b1c0dae9dcb2018ddfa27f236acd1b3cc
Bogdan D <dimitriubogdan23@yahoo.com>
2022-06-03 13:49:50 +0300

    2nd challenge design pattern

---
+++ _3_design_patterns/src/main/java/code/_4_student_effort/_2_strategy/BubbleSort.java
@@ -0,0 +1,15 @@
+package code._4_student_effort._2_strategy;
+
+public class BubbleSort implements SortingStrategy{
+    public void sort(Integer[] list) {
+        for (int i = 0; i <list.length -1; i++){
+            for(int j = i + 1; j < list.length; j++) {
+                if(list[i].compareTo(list[j]) > 0) {
+                    Integer temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+    }
+}
